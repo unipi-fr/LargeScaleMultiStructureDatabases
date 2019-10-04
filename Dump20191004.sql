d07c46a772187f977fae85485d123af769976719
Aleesandro Madonna <mazzorca96@gmail.com>
2019-10-04 17:38:13 +0200

    Database

---
+++ Dump20191004.sql
@@ -0,0 +1,171 @@
+CREATE DATABASE  IF NOT EXISTS `movieDB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `movieDB`;
+-- MySQL dump 10.13  Distrib 8.0.17, for macos10.14 (x86_64)
+--
+-- Host: 127.0.0.1    Database: movieDB
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Acting`
+--
+
+DROP TABLE IF EXISTS `Acting`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Acting` (
+  `actorFK` int(11) NOT NULL,
+  `filmFK` int(11) NOT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  KEY `fk_Acting_Actor1_idx` (`actorFK`),
+  KEY `fk_Acting_Film1_idx` (`filmFK`),
+  CONSTRAINT `fk_Acting_Actor1` FOREIGN KEY (`actorFK`) REFERENCES `actor` (`idActor`),
+  CONSTRAINT `fk_Acting_Film1` FOREIGN KEY (`filmFK`) REFERENCES `film` (`idFilm`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Acting`
+--
+
+LOCK TABLES `Acting` WRITE;
+/*!40000 ALTER TABLE `Acting` DISABLE KEYS */;
+INSERT INTO `Acting` VALUES (4,2,'Protagonist'),(3,1,'Protagonist'),(2,3,'Co-protagonist'),(1,4,'Protagonist'),(1,3,'Protagonist');
+/*!40000 ALTER TABLE `Acting` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Actor`
+--
+
+DROP TABLE IF EXISTS `Actor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Actor` (
+  `idActor` int(11) NOT NULL,
+  `firstName` varchar(45) NOT NULL,
+  `secondName` varchar(45) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idActor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Actor`
+--
+
+LOCK TABLES `Actor` WRITE;
+/*!40000 ALTER TABLE `Actor` DISABLE KEYS */;
+INSERT INTO `Actor` VALUES (1,'Leonardo','Di Caprio',44),(2,'Mia','Khalifa',26),(3,'Zlatan','Ibrahimovic',38),(4,'Nino','Frassica',NULL);
+/*!40000 ALTER TABLE `Actor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Film`
+--
+
+DROP TABLE IF EXISTS `Film`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Film` (
+  `idFilm` int(11) NOT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `publishDate` date DEFAULT NULL,
+  `category` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idFilm`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Film`
+--
+
+LOCK TABLES `Film` WRITE;
+/*!40000 ALTER TABLE `Film` DISABLE KEYS */;
+INSERT INTO `Film` VALUES (1,'Joker','0002-10-19','Drama'),(2,'Avengers','2025-04-19','Action'),(3,'Wanted','2007-12-17','Horror'),(4,'Albakiara','2008-10-24','Drama');
+/*!40000 ALTER TABLE `Film` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Rating`
+--
+
+DROP TABLE IF EXISTS `Rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Rating` (
+  `userFK` varchar(45) NOT NULL,
+  `filmFK` int(11) NOT NULL,
+  `rate` varchar(45) DEFAULT NULL,
+  KEY `fk_Rating_User_idx` (`userFK`),
+  KEY `fk_Rating_Film1_idx` (`filmFK`),
+  CONSTRAINT `fk_Rating_Film1` FOREIGN KEY (`filmFK`) REFERENCES `film` (`idFilm`),
+  CONSTRAINT `fk_Rating_User` FOREIGN KEY (`userFK`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Rating`
+--
+
+LOCK TABLES `Rating` WRITE;
+/*!40000 ALTER TABLE `Rating` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Rating` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `User` (
+  `username` varchar(45) NOT NULL,
+  `fisrtName` varchar(45) NOT NULL,
+  `secondName` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES ('andrè','Andrea','Tubak','andrea@boh.com','1234'),('DaEliminare','boh','boh^2','tizio@caio.sempronio','inutile'),('fraronck','Francesco','Ronchieri','francesco@hotmail.mo','56789'),('mazzorca','Alessandro','Madonna','mazzorca96@gmail.com','pass1'),('stefanoP','Stefano','Petrocchi','stefanoP@gmeil.com','tizio1');
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'movieDB'
+--
+
+--
+-- Dumping routines for database 'movieDB'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-04 17:34:44
