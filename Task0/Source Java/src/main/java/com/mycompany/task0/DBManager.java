7f008a691e946e5ed8eeb5813bf54d179ce17e43
AndreaTBK <56165109+AndreaTBK@users.noreply.github.com>
2019-10-07 16:57:28 +0200

    base classi DBManager e PisaFlix

---
+++ Task0/Source Java/src/main/java/com/mycompany/task0/DBManager.java
@@ -0,0 +1,26 @@
+package com.mycompany.task0;
+
+
+
+
+public class DBManager {
+    
+    private String ip, port, dbName, dbUser, dbPassword;
+    
+    
+    //public User retreiveUser(String username){}
+    
+    //public Film retreiveFilm(String titleFilter){}
+    
+    //public List<Film> retreiveAllFilms(){}
+    
+    //public Rate retreiveRating(String username, String filmTitle){}
+    
+    //public void deleteRating(String username, String filmTitle){}
+    
+    //public void insertOrUpdateRating(String username, String filmTitle, int rate){}
+
+    //public int insertUser(User user){}
+    
+    //public void deleteUser(String username){}
+}
