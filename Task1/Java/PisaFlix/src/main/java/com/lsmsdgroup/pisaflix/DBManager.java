3221da776083c41e1bca5c7a6ac69a1de3a45c7a
Aleesandro Madonna <mazzorca96@gmail.com>
2019-10-25 14:40:53 +0200

    Inting Stefano

--- Task1/Java/PisaFlix/src/main/java/com/lsmsdgroup/pisaflix/DBManager.java
+++ Task1/Java/PisaFlix/src/main/java/com/lsmsdgroup/pisaflix/DBManager.java
@@ -11,7 +11,7 @@ public class DBManager {
     private static EntityManager entityManager;
 
     public static void setup() {
-        factory = Persistence.createEntityManagerFactory("pisaflixdb");
+        factory = Persistence.createEntityManagerFactory("PisaFlix");
 
     }
 
