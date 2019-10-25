e4d232a0d2b7ece5fa294101b45c8e3e8c3ca8ee
Stefano Petrocchi <56197706+StePetro@users.noreply.github.com>
2019-10-25 15:28:31 +0200

    Tolto il superfluo

--- Task1/Java/PisaFlix/src/main/java/com/lsmsdgroup/pisaflix/User.java
+++ Task1/Java/PisaFlix/src/main/java/com/lsmsdgroup/pisaflix/User.java
@@ -1,28 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.lsmsdgroup.pisaflix;
 
 import java.io.Serializable;
 import java.util.Collection;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-/**
- *
- * @author alessandromadonna
- */
 @Entity
 @Table(name = "User")
 @NamedQueries({
