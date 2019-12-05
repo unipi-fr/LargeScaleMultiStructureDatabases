a95fd26ea7e3bb90fef3f2305969c3ad6bd4354e
Andrea <andrea.klaus.tubak@gmail.com>
2019-12-05 09:53:03 +0100

    ...

--- Scraping/Scraping/src/main/java/com/mycompany/scraping/Main.java
+++ Scraping/Scraping/src/main/java/com/mycompany/scraping/Main.java
@@ -1,13 +1,20 @@
 
 package com.mycompany.scraping;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
 
 public class Main {
 
     
     public static void main(String[] args) {
         
-        
+        HttpResponse response = Unirest.post(API_URL)
+        .header("X-RapidAPI-Key", API_KEY)
+        .queryString("parameter", "value")
+        .field("parameter", "value")
+        .asJson();
         
     }
     
