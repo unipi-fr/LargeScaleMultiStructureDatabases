bef1d2a488e84130a81464ba95d69e78cbdb2205
Andrea <andrea.klaus.tubak@gmail.com>
2019-12-15 17:31:08 +0100

    Prima pulizia codice
    Attenzione all'errore occasionale all'avvio

--- Task1/Java/PisaFlix/src/main/java/com/lsmsdbgroup/pisaflix/Entities/Comment.java
+++ Task1/Java/PisaFlix/src/main/java/com/lsmsdbgroup/pisaflix/Entities/Comment.java
@@ -2,46 +2,25 @@ package com.lsmsdbgroup.pisaflix.Entities;
 
 import java.io.Serializable;
 import java.util.*;
-import javax.persistence.*;
 
-@Entity
-@Table(name = "Comment")
+
 public class Comment implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @Column(name = "idComment")
+    
     private Integer idComment;
-
-    @Basic(optional = false)
-    @Column(name = "timestamp")
-    @Temporal(TemporalType.TIMESTAMP)
+    
     private Date timestamp;
-
-    @Basic(optional = false)
-    @Lob
-    @Column(name = "text")
+    
     private String text;
-
-    @JoinTable(name = "Cinema_has_Comment", joinColumns = {
-        @JoinColumn(name = "idComment", referencedColumnName = "idComment")}, inverseJoinColumns = {
-        @JoinColumn(name = "idCinema", referencedColumnName = "idCinema")})
-    @ManyToMany(fetch = FetchType.EAGER)
+   
     private Set<Cinema> cinemaSet = new LinkedHashSet<>();
-
-    @JoinTable(name = "Film_has_Comment", joinColumns = {
-        @JoinColumn(name = "idComment", referencedColumnName = "idComment")}, inverseJoinColumns = {
-        @JoinColumn(name = "idFilm", referencedColumnName = "idFilm")})
-    @ManyToMany(fetch = FetchType.EAGER)
+    
     private Set<Film> filmSet = new LinkedHashSet<>();
-
-    @JoinColumn(name = "idUser", referencedColumnName = "idUser")
-    @ManyToOne(fetch = FetchType.EAGER)
+    
     private User user;
 
+    
     public Comment() {
     }
 
