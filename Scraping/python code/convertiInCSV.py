96fb9ec05e83afcd51a20178f27ca6cb89c697f0
Andrea <andrea.klaus.tubak@gmail.com>
2020-01-01 17:58:05 +0100

    ....

--- Scraping/python code/convertiInCSV.py
+++ Scraping/python code/convertiInCSV.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('UsersPolished.tsv', encoding='utf-8') as tsvfile:
+with open('movies_metadata.tsv', encoding='utf-8') as tsvfile:
     reader = csv.reader(tsvfile, dialect='excel-tab')
   
     lista = []
@@ -8,7 +8,7 @@ with open('UsersPolished.tsv', encoding='utf-8') as tsvfile:
     for row in reader:
         lista.append(row)
 
-with open('UsersPolished.csv', mode='w' , newline = '') as csv_file:
+with open('movies_metadata.csv', mode='w' , newline = '', encoding="utf-8") as csv_file:
     CSVwriter = csv.writer(csv_file, delimiter=',')
 
     CSVwriter.writerows(lista)
