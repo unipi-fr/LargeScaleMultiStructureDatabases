b2bb43c959c50d87e9949a8d93b367f569581597
Andrea <andrea.klaus.tubak@gmail.com>
2020-01-13 17:21:33 +0100

    Sviluppate query per modificare formato campi critici + pulizia nuovo dataset film

--- Scraping/python code/convertiInTSV.py
+++ Scraping/python code/convertiInTSV.py
@@ -1,13 +1,13 @@
 import csv
 
 
-with open('../Data/FilmsEsportatiEsempio.csv', encoding="utf8") as csvfile:
+with open('../Data/wiki_movies.csv', encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     lista = []
 
     for row in reader:
         lista.append(row)
 
-with open('../Data/FilmsEsportatiEsempio.tsv', mode= 'w', newline= '', encoding="utf8") as tsv_file:
+with open('../Data/wiki_movies.tsv', mode= 'w', newline= '', encoding="utf8") as tsv_file:
     TSVWriter = csv.writer(tsv_file, dialect='excel-tab')
     TSVWriter.writerows(lista)
--- Scraping/python code/manipolazioneFormatoFilm.py
+++ Scraping/python code/manipolazioneFormatoFilm.py
@@ -14,7 +14,7 @@ def parse_date(text):
 
 
 
-with open('../Data/movies.tsv', encoding="utf8") as tsvfile:
+with open('../Data/wiki_movies.tsv', encoding="utf8") as tsvfile:
     reader = csv.reader(tsvfile, dialect='excel-tab')
 
     lista = []
@@ -25,55 +25,11 @@ with open('../Data/movies.tsv', encoding="utf8") as tsvfile:
 for i in range(len(lista)):
     if i == 0:
         continue
-    if lista[i][6] == '':
-        lista[i][6] = datetime.date(randint(1899,2015), randint(1,12),randint(1,28)).isoformat()
-    else:
-        data = parse_date(lista[i][6])
-        lista[i][6] = data.isoformat()
+    data = parse_date(lista[i][0])
+    lista[i][0] = data.isoformat()
 
-with open('../Data/movies_dateISO.tsv', mode='w' , newline = '', encoding="utf8") as movies_dateISO:
+with open('../Data/wiki_movies_dateISO.tsv', mode='w' , newline = '', encoding="utf8") as movies_dateISO:
     writer = csv.writer(movies_dateISO, dialect='excel-tab')
 
     writer.writerows(lista)
 
-'''
-salvo in una lista tutte le righe
-
-faccio un for che ignora la prima riga e modifica la stringa in posizione 6 con la isodate
-
-scrivo tutte le righe in un nuovo file tsv
-
-
-    lista = []
-    i = 0
-    for row in reader:
-        if i == 0:
-            i+= 1
-            continue
-        dataConvertita = datetime.datetime.strptime(row[6], "%d/%m/%Y")
-        print(str(dataConvertita) + "\t" + str(dataConvertita.isoformat()))
-        if i == 5:
-            break
-        i += 1
-
-i = 1
-lista_senza_duplicati = []
-
-while i < len(lista):
-    id_corrente = lista[i][0]
-    id_precedente = lista[i-1][0]
-
-    if id_corrente != id_precedente:
-        lista_senza_duplicati.append(lista[i])
-    i += 1
-
-with open('InfoTitoliPuliti.tsv', mode='w' , newline = '') as titoli_file:
-    titoli_writer = csv.writer(titoli_file, dialect='excel-tab')
-
-    titoli_writer.writerows(lista_senza_duplicati)
-
-
-
-
-
-'''
