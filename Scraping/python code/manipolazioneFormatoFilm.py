6a86bd4cc2e9932fa5f54d6caafa4fe5d0cb25c3
Andrea <andrea.klaus.tubak@gmail.com>
2020-01-16 10:05:18 +0100

    Update manipolazioneFormatoFilm.py

--- Scraping/python code/manipolazioneFormatoFilm.py
+++ Scraping/python code/manipolazioneFormatoFilm.py
@@ -3,17 +3,21 @@ import csv
 import datetime
 from random import randint
 import string
+import dateutil.parser
+
+
+
 
 def remove_non_printable(s):
     return ''.join(c for c in s if c in string.printable)
 
 
 def parse_date(text):
-    for frm in ("%d/%m/%Y", "%Y-%m-%d"):
-        try:
-            return datetime.datetime.strptime(text, frm)
-        except ValueError:
-            pass
+    
+    try:
+        return dateutil.parser.parse(text).year
+    except ValueError:
+        pass
     print(text)
     raise ValueError("Non Funziona nulla!!!!")
 
@@ -30,16 +34,8 @@ with open('../Data/wiki_movies.tsv', encoding="utf8") as tsvfile:
 for i in range(len(lista)):
     if i == 0:
         continue
-    if lista[i][2] == "Unknown": 
-        lista[i][2] = ''
-    
-    if lista[i][3] == "Unknown": 
-        lista[i][3] = ''
-    if lista[i][4] == "unknown": 
-        lista[i][4] = ''
-    
-    #descrizione = lista[i][5].translate({ord(i): None for i in '\n'})
-    #lista[i][5] = descrizione
+    data = 
+    lista[i][0] = anno
 
 with open('../Data/wiki_movies_stripped.tsv', mode='w' , newline = '', encoding="utf8") as movies_dateISO:
     writer = csv.writer(movies_dateISO, dialect='excel-tab')
