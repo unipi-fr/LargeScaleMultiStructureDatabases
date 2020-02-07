754bb33b2a659ce1b062f0b3c2e8a50ee50dc327
Stefano <stefanopetrocchi7@gmail.com>
2020-02-07 11:49:28 +0100

    Update EngageAnalyticController.java

--- Task2/Java/PisaFlix/src/main/java/com/lsmsdbgroup/pisaflixg/EngageAnalyticController.java
+++ Task2/Java/PisaFlix/src/main/java/com/lsmsdbgroup/pisaflixg/EngageAnalyticController.java
@@ -3,11 +3,14 @@ package com.lsmsdbgroup.pisaflixg;
 import com.lsmsdbgroup.pisaflix.AnalyticsClasses.EngageResult;
 import com.lsmsdbgroup.pisaflix.Entities.Film;
 import com.lsmsdbgroup.pisaflix.pisaflixservices.PisaFlixServices;
+import impl.org.controlsfx.autocompletion.AutoCompletionTextFieldBinding;
+import impl.org.controlsfx.autocompletion.SuggestionProvider;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashSet;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.logging.Level;
@@ -27,71 +30,77 @@ import org.controlsfx.control.textfield.TextFields;
 
 public class EngageAnalyticController implements Initializable {
 
+    SuggestionProvider<Film> provider;
+
     @FXML
     private CategoryAxis xAxis;
-    
+
     @FXML
     private NumberAxis yAxis;
-    
+
     @FXML
     private TextField titleTextField;
-    
+
     @FXML
     private ComboBox startCombo;
-    
+
     @FXML
     private ComboBox endCombo;
-    
+
     @FXML
-    private BarChart<String,Number> barChart;
-    
+    private BarChart<String, Number> barChart;
+
     @FXML
     private PieChart pieChart;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        xAxis.setLabel("");       
+        xAxis.setLabel("");
         yAxis.setLabel("Engage");
-        
+
+        Set<Film> autoCompletions = new LinkedHashSet<>();
+        provider = SuggestionProvider.create(autoCompletions);
+        AutoCompletionTextFieldBinding<Film> autoCompletionTextFieldBinding = new AutoCompletionTextFieldBinding<>(titleTextField, provider);
+
         ArrayList years = new ArrayList();
-        for(int i = 1961; i < 2021; i++)
-        {
+        for (int i = 1961; i < 2021; i++) {
             years.add(i);
         }
-        
+
         startCombo.getItems().setAll(years);
         endCombo.getItems().setAll(years);
-        
+
     }
-    
+
     @FXML
-    private void suggestions(){
-        if(titleTextField.getText() != null){
-            Set<Film> films = PisaFlixServices.filmService.getFilmsFiltered(titleTextField.getText(), null, null, 0);
-            TextFields.bindAutoCompletion(titleTextField, films);
+    private void suggestions() {
+        if (titleTextField.getText() != null) {
+            Set<Film> newCompletions = PisaFlixServices.filmService.getFilmsFiltered(titleTextField.getText(), null, null, 0);;
+            provider.clearSuggestions();
+            provider.addPossibleSuggestions(newCompletions);
         }
     }
-    
+
     @FXML
-    private void calculateEngagement(){
-        if(startCombo.getSelectionModel().isEmpty() || endCombo.getSelectionModel().isEmpty()){
+    private void calculateEngagement() {
+        if (startCombo.getSelectionModel().isEmpty() || endCombo.getSelectionModel().isEmpty()) {
             App.printWarningDialog("No Year Selected", "", "You must select both Year");
             return;
         }
-        
+
         String filmTitle = titleTextField.getText();
         Set<Film> films = PisaFlixServices.filmService.getFilmsFiltered(filmTitle, null, null, 0);
-        
+
         Film film = films.iterator().next();
-        
+
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
-        
+
         Integer startYear = (Integer) startCombo.getValue();
         String startYearStr = startYear.toString();
-        
+
         Integer endYear = ((Integer) endCombo.getValue()) + 1;
         String endYearStr = endYear.toString();
-        
+
         Date dStart = null;
         Date dEnd = null;
         try {
@@ -100,39 +109,40 @@ public class EngageAnalyticController implements Initializable {
         } catch (ParseException ex) {
             Logger.getLogger(EngageAnalyticController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         Set<EngageResult> engageResults = PisaFlixServices.analyticService.engagementAnalytics(dStart, dEnd, film);
-        
+
         EngageResult engageResult;
-        if(engageResults != null)
+        if (engageResults != null) {
             engageResult = engageResults.iterator().next();
-        else
+        } else {
             engageResult = new EngageResult(film.getId(), 0L, 0L, 0L);
-        
+        }
+
         setBarChart(engageResult);
         setPieChart(engageResult);
     }
-    
-    private void setBarChart(EngageResult engageResult){
+
+    private void setBarChart(EngageResult engageResult) {
         barChart.getData().clear();
         XYChart.Series series = new XYChart.Series();
-        
-        series.getData().add(new XYChart.Data<>("View",engageResult.getViewCount()));
-        series.getData().add(new XYChart.Data<>("Favourite",engageResult.getFavouriteCount()));
-        series.getData().add(new XYChart.Data<>("Comment",engageResult.getCommentCount()));
-        
+
+        series.getData().add(new XYChart.Data<>("View", engageResult.getViewCount()));
+        series.getData().add(new XYChart.Data<>("Favourite", engageResult.getFavouriteCount()));
+        series.getData().add(new XYChart.Data<>("Comment", engageResult.getCommentCount()));
+
         barChart.getData().addAll(series);
     }
-    
-    private void setPieChart(EngageResult engageResult){
+
+    private void setPieChart(EngageResult engageResult) {
         pieChart.getData().clear();
-                
+
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
                 new PieChart.Data("View", engageResult.getViewCount()),
                 new PieChart.Data("Favourite", engageResult.getFavouriteCount()),
                 new PieChart.Data("Comment", engageResult.getCommentCount())
         );
-        
+
         pieChart.setData(pieChartData);
         pieChart.setLabelsVisible(false);
     }
